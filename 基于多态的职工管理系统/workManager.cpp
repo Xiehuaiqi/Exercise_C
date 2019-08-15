62c90d906c2d26cb0af44e906f9a02b616171367
xiehuaiqi <353850260@qq.com>
2019-08-15 20:39:01 +0800

    基于多态的职工管理系统

---
+++ 基于多态的职工管理系统/workManager.cpp
@@ -0,0 +1,13 @@
+//
+// Created by xhq on 2019/8/15.
+//
+
+#include "workManager.h"
+
+WorkerManager::WorkerManager() {
+
+}
+
+WorkerManager::~WorkerManager() {
+
+}
---
+++ 基于多态的职工管理系统/workManager.h
@@ -0,0 +1,18 @@
+//
+// Created by xhq on 2019/8/15.
+//
+
+#ifndef EXERCISE_C_WORKMANAGER_H
+#define EXERCISE_C_WORKMANAGER_H
+
+#endif //EXERCISE_C_WORKMANAGER_H
+
+#include <iostream>
+using namespace std;//使用标准的命名函数
+#pragma once //防止头文件重复包含
+class WorkerManager{
+public:
+    WorkerManager();
+
+    ~WorkerManager();
+};
